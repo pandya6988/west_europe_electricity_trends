77f600aa108a140c6832aeec9425b9168529e18d
kirtikumarpandya <kirti6988@gmail.com>
2023-07-09 03:49:54 +0200

    add more features.

---
+++ app.py
@@ -0,0 +1,63 @@
+import streamlit as st
+import plotly.express as px
+import pandas as pd
+from src import data_global_preprocessing as dgp
+
+st.set_page_config(layout='wide', initial_sidebar_state='expanded')
+
+with open('style.css') as f:
+    st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+
+st.header("West europe dashboard")
+st.sidebar.header("control panel")
+with st.sidebar:
+    vis_type = st.selectbox(label="Visualization type", options=["Global", "By country"])
+
+df_global = pd.read_csv("west_europe_electricity_data.csv")
+if vis_type == "Global":
+    df_global["date"] = pd.to_datetime(df_global["date"])
+
+    df_global = df_global.groupby('date').agg({'price': 'sum', 'demand': 'sum', "supply": "sum"}).reset_index()
+
+    window = st.sidebar.number_input("Moving window", 2, 1000, step=1, value=365)
+    threshold = st.sidebar.slider(label="zscore threshold to detect outliers",
+                          min_value=1, max_value=5, value=4)
+    moving_avg_year = st.sidebar.number_input(label="Metrics year", min_value=2015, max_value=2023, value=2021)
+
+    df,_ = dgp.get_fig_for_moving_window(df_global=df_global ,window=window)
+
+    df_new = dgp.remove_outliers_zscore(df, threshold=threshold)
+    fig2 = px.line(df, x="date",
+                       y=["supply_moving_avg", "demand_moving_avg", "price"],
+                       hover_name="date", title="Trends").update_xaxes(dtick="M12",  tickformat="%Y" )
+
+    df_new.set_index("date", inplace=True)
+    df_new["hour"] = df_new.index.hour
+    df_new["dayofweek"] = df_new.index.dayofweek
+    df_new["quarter"] = df_new.index.quarter
+    df_new["month"] = df_new.index.month
+    df_new["dayofyear"] = df_new.index.dayofyear
+
+    st.markdown('### Metrics')
+    col1, col2, col3 = st.columns(3)
+
+    price_mean_diff = df_new[df_new.index>f'31-12-{moving_avg_year}']['price'].mean() - df_new[df_new.index<f'31-12-{moving_avg_year}']['price'].mean()
+    col1.metric(f"Price avg from {moving_avg_year}", f"{round(df_new[df_new.index>f'31-12-{moving_avg_year}']['price'].mean(), 2)}€", f"{round(price_mean_diff,2)}€")
+
+    demand_mean_diff = df_new[df_new.index > f'31-12-{moving_avg_year}']['demand'].mean() - df_new[df_new.index <f'31-12-{moving_avg_year}'][
+        'demand'].mean()
+    col2.metric(f"Demand avg from {moving_avg_year}", f"{round(df_new[df_new.index >f'31-12-{moving_avg_year}']['demand'].mean(), 2)}€",
+                f"{round(demand_mean_diff, 2)}€")
+    supply_mean_diff = df_new[df_new.index >f'31-12-{moving_avg_year}']['supply'].mean() - df_new[df_new.index <f'31-12-{moving_avg_year}'][
+        'supply'].mean()
+    col3.metric(f"Supply avg from {moving_avg_year}", f"{round(df_new[df_new.index >f'31-12-{moving_avg_year}']['supply'].mean(), 2)}€",
+                f"{round(supply_mean_diff, 2)}€")
+    st.plotly_chart(fig2, use_container_width=True)
+
+    st.markdown("### Detailed graph")
+
+    col1, col2 = st.columns(2)
+    time = col1.selectbox(label="Time parameter", options=["hour", "dayofweek", "quarter", "month", "dayofyear"])
+    feature = col2.selectbox("select the feature", ["demand", "supply", "price"])
+    fig2 = px.box(df_new, x=time, y=feature, title=f"{time} → {feature} box plot")
+    st.plotly_chart(fig2, use_container_width=True)
---
+++ src/data_global_preprocessing.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import plotly.express as px
+import numpy as np
+import streamlit as st
+from scipy.stats import zscore
+
+df_global = pd.read_csv("west_europe_electricity_data.csv")
+df_global["date"] = pd.to_datetime(df_global["date"])
+
+df_global = df_global.groupby('date').agg({'price': 'sum', 'demand': 'sum', "supply":"sum"}).reset_index()
+
+def get_fig_for_moving_window(df_global=df_global, window=365):
+    df_global[f"demand_moving_avg"] = df_global["demand"].rolling(window=window).mean()
+    df_global[f"supply_moving_avg"] = df_global["supply"].rolling(window=window).mean()
+    df_global[f"price_moving_avg"] = df_global["price"].rolling(window=window).mean()
+
+    fig = px.line(df_global, x="date", y=["supply_moving_avg", "demand_moving_avg", "price"],
+                  hover_name="date", title="Global trends").update_xaxes(dtick="M12", tickformat="%Y")
+    return df_global, fig
+
+
+def remove_outliers_zscore(df=df_global, series="price", threshold=4):
+    z_scores = zscore(df[series].to_numpy())
+
+    # df = df[np.abs(z_scores) < threshold]
+    # df[series].where(np.abs(z_scores) > threshold, df[series].shift())
+
+    last_valid = None
+    for idx, value in enumerate(df[series]):
+        if np.abs(z_scores[idx]) > threshold:
+            if last_valid is not None:
+                df.loc[idx, series] = last_valid
+        else:
+            last_valid = value
+    return df
